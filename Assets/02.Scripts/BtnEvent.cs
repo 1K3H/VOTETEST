87812066a31376c3d6f55f126d1af10469464d6b
1K3H <dkadowlwh@naver.com>
2021-11-21 16:05:28 +0900

    버튼 이벤트에 상태변수 변경 적용

--- Assets/02.Scripts/BtnEvent.cs
+++ Assets/02.Scripts/BtnEvent.cs
@@ -32,12 +32,39 @@ public class BtnEvent : MonoBehaviour
     public void OpenVoteListPanel()
     {
         // 투표와 관련된 어떤 창도 떠있지 않은 경우와 voteListPanel이 떠있는 경우에만 작동한다. (조건)
-        voteListPanel.SetActive(!voteListPanel.activeSelf);
+        if(mode == Mode.CLOSED)
+        {
+            voteListPanel.SetActive(true);
+            mode = Mode.LIST;
+        }
+        else if(mode == Mode.LIST)
+        {
+            voteListPanel.SetActive(false);
+            mode = Mode.CLOSED;
+        }
+    }
+
+    public void OpenVoteCreatePanel()
+    {
+        mode = Mode.CREATE;
+    }
+
+    public void FinishVoteCreate()
+    {
+        // 생성한 투표 데이터 DB로 전송하기
+        // ListPanel로 돌아가기
     }
 
     public void OpenVotePanel()
     {
         voteListPanel.SetActive(false);
         votePanel.SetActive(true);
+        mode = Mode.VOTE;
+    }
+
+    public void FinishVote()
+    {
+        // 투표 정보 DB에 업데이트하기
+        // ListPanel로 돌아가기
     }
 }
