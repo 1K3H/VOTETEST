6f71e361a73e13b81e59479eb5922eaaad28e4ba
1K3H <dkadowlwh@naver.com>
2021-11-26 18:06:32 +0900

    투표 비율 결과 표시 구현

--- Assets/02.Scripts/VoteManager.cs
+++ Assets/02.Scripts/VoteManager.cs
@@ -16,6 +16,12 @@ public class VoteManager : MonoBehaviour
     public GameObject voteListBlock;
     public GameObject voteCreatePanel;
     public GameObject votePanel;
+    public int SelectedVoteID;
+    public int SelectedSelectionNum;
+
+    public Text inputTitle;
+    public Text inputSelection1;
+    public Text inputSelection2;
 
     TextAsset voteDataText;
     MyVoteDataArray myVoteData;
@@ -62,6 +68,34 @@ public class VoteManager : MonoBehaviour
         Debug.Log("LoadAllData");
     }
 
+    public void SetVoteList()
+    {
+        for (int i = 0; i < myVoteData.data.Length; i++)
+        {
+            int idIndex = i;
+            GameObject voteBlock = Instantiate(voteListBlock, voteListPanelContent.transform, false);
+            // Set PosY
+            voteBlock.GetComponent<RectTransform>().anchoredPosition = new Vector2(voteBlock.GetComponent<RectTransform>().anchoredPosition.x, voteBlock.GetComponent<RectTransform>().anchoredPosition.y - 80 * i);
+            // Set Title
+            voteBlock.transform.Find("Text Title").GetComponent<Text>().text = myVoteData.data[i].TITLE;
+            // Set Info
+            voteBlock.transform.Find("Text Info").GetComponent<Text>().text = $"제작 : {myVoteData.data[i].USER_NAME}   참여 : {myVoteData.data[i].SELECTION[0].COUNT + myVoteData.data[i].SELECTION[1].COUNT}";
+            // Set Date
+            voteBlock.transform.Find("Text Date").GetComponent<Text>().text = myVoteData.data[i].CREATE_DATE;
+            // Set button event
+            voteBlock.GetComponent<Button>().onClick.AddListener(() => OpenVotePanel(myVoteData.data[idIndex].ID));
+        }
+        Debug.Log("SetVoteList");
+    }
+
+    public void DeleteVoteList()
+    {
+        for (int i = 0; i < voteListPanelContent.transform.childCount; i++)
+        {
+            Destroy(voteListPanelContent.transform.GetChild(i).gameObject);
+        }
+    }
+
     public void InsertData(string title, string selection1, string selection2)
     {
         VoteData newVoteData = new VoteData();
@@ -93,33 +127,6 @@ public class VoteManager : MonoBehaviour
         Debug.Log("InsertData");
     }
 
-    public void DeleteVoteList()
-    {
-        for (int i = 0; i < voteListPanelContent.transform.childCount; i++)
-        {
-            Destroy(voteListPanelContent.transform.GetChild(i).gameObject); 
-        }
-    }
-
-    public void SetVoteList()
-    {
-        for (int i = 0; i < myVoteData.data.Length; i++)
-        {
-            int idIndex = i;
-            GameObject voteBlock = Instantiate(voteListBlock, voteListPanelContent.transform, false);
-            // Set PosY
-            voteBlock.GetComponent<RectTransform>().anchoredPosition = new Vector2(voteBlock.GetComponent<RectTransform>().anchoredPosition.x, voteBlock.GetComponent<RectTransform>().anchoredPosition.y - 80 * i);
-            // Set Title
-            voteBlock.transform.Find("Text Title").GetComponent<Text>().text = myVoteData.data[i].TITLE;
-            // Set Info
-            voteBlock.transform.Find("Text Info").GetComponent<Text>().text = $"제작 : {myVoteData.data[i].USER_NAME}   참여 : {myVoteData.data[i].SELECTION[0].COUNT + myVoteData.data[i].SELECTION[1].COUNT}";
-            // Set Date
-            voteBlock.transform.Find("Text Date").GetComponent<Text>().text = myVoteData.data[i].CREATE_DATE;
-            // Set button event
-            voteBlock.GetComponent<Button>().onClick.AddListener(() => OpenVotePanel(myVoteData.data[idIndex].ID));
-        }
-        Debug.Log("SetVoteList");
-    }
 
     // Button Events
 
@@ -147,9 +154,9 @@ public class VoteManager : MonoBehaviour
     public void FinishVoteCreate()
     {
         // 생성한 투표 데이터 DB로 전송하기
-        string title = voteCreatePanel.transform.Find("InputField Title/Text").GetComponent<Text>().text;
-        string selection1 = voteCreatePanel.transform.Find("InputField Selection1/Text").GetComponent<Text>().text;
-        string selection2 = voteCreatePanel.transform.Find("InputField Selection2/Text").GetComponent<Text>().text; 
+        string title = inputTitle.text;
+        string selection1 = inputSelection1.text;
+        string selection2 = inputSelection2.text;
         InsertData(title, selection1, selection2);
         DeleteVoteList();
         SetVoteList();
@@ -158,6 +165,7 @@ public class VoteManager : MonoBehaviour
 
     public void OpenVotePanel(int ID)
     {
+        SelectedVoteID = ID;
         // Set selection
         for (int i = 0; i < myVoteData.data[ID].SELECTION.Length; i++)
         {
@@ -173,11 +181,38 @@ public class VoteManager : MonoBehaviour
         mode = Mode.VOTE;
     }
 
+    public void SelectSelection(int selectionNum)
+    {
+        SelectedSelectionNum = selectionNum;
+        Debug.Log(SelectedSelectionNum);
+    }
+
     public void FinishVote()
     {
         // 투표 정보 DB에 업데이트하기
-        LoadAllData();
-        BackToVoteListPanel();
+        myVoteData.data[SelectedVoteID].SELECTION[SelectedSelectionNum].COUNT++;
+        // DeleteVoteList();
+        // SetVoteList();
+
+        int allCount = 0;
+
+        for(int i = 0; i < myVoteData.data[SelectedVoteID].SELECTION.Length; i++)
+        {
+            allCount += myVoteData.data[SelectedVoteID].SELECTION[i].COUNT;
+        }
+
+        for(int i = 0; i < myVoteData.data[SelectedVoteID].SELECTION.Length; i++)
+        {
+            float per = 100 * ((float)myVoteData.data[SelectedVoteID].SELECTION[i].COUNT / (float)allCount);
+
+            votePanel.transform.Find($"Panel Selection{i+1}/Text Count").GetComponent<Text>().text = $"{myVoteData.data[SelectedVoteID].SELECTION[i].COUNT} / {per}%";
+
+            // Debug.Log(allCount);
+            // Debug.Log(myVoteData.data[SelectedVoteID].SELECTION[i].COUNT);
+
+            votePanel.transform.Find($"Panel Selection{i+1}/Text Count").GetComponent<Text>().gameObject.SetActive(true);
+        }
+        mode = Mode.VOTE_RESULT;
     }
 
     public void BackToVoteListPanel()
